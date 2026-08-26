8dbcd88d801cc7a4dca88362e10eff0309011aca
mxfax <116275963+mxfax@users.noreply.github.com>
2026-08-26 15:36:59 +0100

    Delete check_models.py

--- check_models.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyDV1kwK0KcBOnInOpdOwutuSAuqMgLq82E")
-
-print("Listing available models...")
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
